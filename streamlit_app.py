539ab730efa24f93c698d9e49c47b9c5304f5df9
Pragya Shree <pragyashree1667@email.com>
2026-06-30 00:43:03 +0530

    Add recommendation UI

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,26 @@
 import streamlit as st
 import pickle
 
-st.set_page_config(page_title="Movie Recommender", layout="wide")
-
 st.title("🎬 Movie Recommendation System")
 
-# Load data
 movies = pickle.load(open("movies.pkl", "rb"))
 similarity = pickle.load(open("similarity.pkl", "rb"))
 
-st.success("App is running successfully 🚀")
+movie_list = movies['title'].values
+
+selected_movie = st.selectbox("Select a movie", movie_list)
+
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = similarity[index]
+    movie_indices = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommendations = []
+    for i in movie_indices:
+        recommendations.append(movies.iloc[i[0]].title)
+    return recommendations
+
+if st.button("Recommend"):
+    results = recommend(selected_movie)
+    for i in results:
+        st.write("👉", i)

7527942d5e3736148e816b6674eea48a9c53004a
Pragya Shree <pragyashree1667@email.com>
2026-06-30 00:45:03 +0530

    Restore poster functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,40 @@
 import streamlit as st
 import pickle
+import requests
 
 st.title("🎬 Movie Recommendation System")
 
 movies = pickle.load(open("movies.pkl", "rb"))
 similarity = pickle.load(open("similarity.pkl", "rb"))
 
-movie_list = movies['title'].values
-
-selected_movie = st.selectbox("Select a movie", movie_list)
+def fetch_poster(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=YOUR_API_KEY"
+    data = requests.get(url).json()
+    return "https://image.tmdb.org/t/p/w500" + data["poster_path"]
 
 def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
     distances = similarity[index]
-    movie_indices = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
 
-    recommendations = []
-    for i in movie_indices:
-        recommendations.append(movies.iloc[i[0]].title)
-    return recommendations
+    recommended_movies = []
+    posters = []
+
+    for i in movie_list:
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movies.append(movies.iloc[i[0]].title)
+        posters.append(fetch_poster(movie_id))
+
+    return recommended_movies, posters
+
+movie_list = movies['title'].values
+selected_movie = st.selectbox("Select a movie", movie_list)
 
 if st.button("Recommend"):
-    results = recommend(selected_movie)
-    for i in results:
-        st.write("👉", i)
+    names, posters = recommend(selected_movie)
+
+    cols = st.columns(5)
+    for i in range(5):
+        with cols[i]:
+            st.text(names[i])
+            st.image(posters[i])

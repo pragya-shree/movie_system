58e403afe74a5fa4849fd1a67d1929d11670e27e
Pragya Shree <pragyashree1667@email.com>
2026-06-29 23:54:47 +0530

    Clean project for deployment

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: sh setup.sh && streamlit run mod1.py
--- setup.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p ~/.streamlit/
-
-cat > ~/.streamlit/config.toml <<EOF
-[server]
-port = $PORT
-enableCORS = false
-headless = true
-EOF
